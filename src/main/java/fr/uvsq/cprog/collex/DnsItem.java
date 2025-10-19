cec734eb4aa3f63876021ef5f9784c5255d1b666
uvsq22200538 <sylvain.helain@gmail.com>
2025-10-20 00:03:55 +0200

    Ajout du construteur de DnsItem

--- src/main/java/fr/uvsq/cprog/collex/DnsItem.java
+++ src/main/java/fr/uvsq/cprog/collex/DnsItem.java
@@ -8,4 +8,13 @@ package fr.uvsq.cprog.collex;
  */
 
 public class DnsItem {
+
+    NomMachine nom;
+    AdresseIP ip;
+
+    DnsItem(String nom, String ip) {
+        this.nom = new NomMachine(nom);
+        this.ip = new AdresseIP(ip);
+
+    }
 }
